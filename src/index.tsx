4b387a5f174f100ed7b460f2ed7561da9f5a885e
Anthalasath <chapchico@live.com>
2022-03-20 19:56:16 +0100

    display achievements and make list filterable

--- src/index.tsx
+++ src/index.tsx
@@ -5,65 +5,189 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Avatar, Grid, List, ListItem, ListItemButton, ListItemText, Paper, Stack, TextField } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { flatten } from "lodash"
+
+const cosCsharpDeveloperJob: Job = {
+  title: "C# Programmer",
+  description: "Develop UI and tools for the game Clash Of Streamers"
+}
+
+const cosProject: Project = {
+  name: "Clash Of Streamers",
+  teamDisplayName: "The Singularity Group"
+}
+
+interface MockAchievementInput {
+  skill: string,
+  job: Job,
+  project: Project,
+  description: string,
+  dateOfDelivery: Date,
+}
+
+class MockAchievementFactory {
+  static lastId = 0;
+
+  static createCoS(description: string): Achievement {
+    return this.create({
+      description,
+      skill: "C#",
+      job: cosCsharpDeveloperJob,
+      project: cosProject,
+      dateOfDelivery: new Date(Date.parse("31 May 2022"))
+    });
+  }
+
+  static createAutomation(description: string, project: Project, dateOfDelivery: Date): Achievement {
+    return this.create({
+      description,
+      skill: "NodeJS",
+      job: {
+        title: "Automation developer",
+        description: "Develop automated internal tools"
+      },
+      project: {
+        name: "DUBIex bot",
+        teamDisplayName: "The Singularity Group"
+      },
+      dateOfDelivery: new Date(Date.parse("31 June 2018"))
+    });
+  }
+
+  static create(input: MockAchievementInput): Achievement {
+    const id = (++this.lastId).toString();
+    return {
+      id,
+      skill: input.skill,
+      project: input.project,
+      description: input.description,
+      dateOfDelivery: input.dateOfDelivery,
+      job: input.job
+    }
+  }
+}
 
 const profiles: Profile[] = [
   {
+    id: "1",
     handle: "Anthalasath",
     imageUri:
       "https://pbs.twimg.com/profile_images/1297158984730902528/5cGwqw-I_400x400.png",
     skills: [
-      { name: "C#", achievementsCount: 34 },
-      { name: "NodeJS", achievementsCount: 12 },
-      { name: "Python", achievementsCount: 7 },
-      { name: "Unity", achievementsCount: 35 },
-      { name: "CSS", achievementsCount: 6 },
-      { name: "React", achievementsCount: 10 },
-      { name: "Git", achievementsCount: 10 },
-      { name: "Solidity", achievementsCount: 10 }
+      {
+        name: "C#",
+        achievements: [
+          MockAchievementFactory.createCoS("Developed the HotStories feature"),
+          MockAchievementFactory.createCoS("Developed the Hyperlinks feature and API"),
+          MockAchievementFactory.createCoS("Developed the support ticket feature and API"),
+          MockAchievementFactory.createCoS("Developed the HotStories posts API"),
+          MockAchievementFactory.createCoS("Optimized code across the HotStories feature"),
+          MockAchievementFactory.createCoS("Optimized code across the Hyperlinks feature")
+        ]
+      },
+      {
+        name: "NodeJS", achievements: [
+          MockAchievementFactory.createAutomation("Developed the DUBIex trading bot",
+            {
+              name: "DUBIex bot",
+              teamDisplayName: "The Singularity Group"
+            },
+            new Date(Date.parse("31 June 2018"))
+          ),
+          MockAchievementFactory.createAutomation("Developed a fraudulent transaction detection system",
+            {
+              name: "Gaming For Good",
+              teamDisplayName: "The Singularity Group"
+            },
+            new Date(Date.parse("31 June 2018"))
+          )
+        ]
+      },
+      {
+        name: "Solidity", achievements: [
+          MockAchievementFactory.create({
+            skill: "Solidity",
+            project: {
+              name: "DUBIex",
+              teamDisplayName: "The Singularity Group"
+            },
+            description: "Audited smart contracts",
+            dateOfDelivery: new Date(Date.parse("31 May 2018")),
+            job: {
+              title: "Generalist Programmer",
+              description: "Help out across a variety of projects and technologies"
+            }
+          })
+        ]
+      },
+      { name: "Git", achievements: [] },
     ]
   },
 ];
+interface Project {
+  name: string,
+  teamDisplayName: string
+}
+
+interface Job {
+  title: string,
+  description: string
+}
+
+interface Achievement {
+  skill: string,
+  job: Job,
+  project: Project,
+  description: string,
+  dateOfDelivery: Date,
+  id: string
+}
 
 interface Skill {
   name: string,
-  achievementsCount: number
+  achievements: Achievement[]
 }
 
 interface Profile {
+  id: string,
   handle: string,
   imageUri: string,
   skills: Skill[]
 }
 
-async function getProfile(handle): Promise<Profile> {
+function getLatestAchievements(profile: Profile): Achievement[] {
+  const achievements: Achievement[] = flatten(Object.keys(profile.skills).map(k => profile.skills[k].achievements));
+  return achievements.sort(a => a.dateOfDelivery.getTime());
+}
+
+async function getProfile(handle: string): Promise<Profile> {
   return profiles.find((p) => p.handle === handle);
 }
 
-function Skills(props: { skills: Skill[] }) {
+function SkillList(props: { skills: Skill[], handleClick: (skill: string) => void }) {
   return <Paper style={{ maxHeight: 400, maxWidth: 400, overflow: "auto" }}><List>
     {props.skills
-      .sort(skill => 0 - skill.achievementsCount)
-      .map(skill => <SkillListItemView key={skill.name} skill={skill}></SkillListItemView>)
+      .sort(skill => 0 - skill.achievements.length)
+      .map(skill => <SkillListItemView key={skill.name} skill={skill} handleClick={props.handleClick}></SkillListItemView>)
     }
   </List>
   </Paper>
 }
 
-function SkillListItemView(props: { skill: Skill }) {
+function SkillListItemView(props: { skill: Skill, handleClick: (skill: string) => void }) {
   return <ListItem>
-    <ListItemButton>
-      <ListItemText primary={`${props.skill.name} (${props.skill.achievementsCount})`}></ListItemText>
+    <ListItemButton onClick={() => props.handleClick(props.skill.name)}>
+      <ListItemText primary={`${props.skill.name} (${props.skill.achievements.length})`}></ListItemText>
     </ListItemButton>
   </ListItem>
 }
 
-function ProfileView(props) {
+function ProfileView(props: { profile: Profile, handleSkillClick: (skill: string) => void }) {
   return (
     <Stack spacing={2} alignItems="center">
       <h1>{props.profile.handle}</h1>
       <Avatar alt="avatar" src={props.profile.imageUri} sx={{ height: 150, width: 150 }}></Avatar>
-      <Skills skills={props.profile.skills}></Skills>
+      <SkillList skills={props.profile.skills} handleClick={props.handleSkillClick}></SkillList>
     </Stack>
   );
 }
@@ -79,18 +203,54 @@ function ProfileSearchField(props: { handleChange: (value: any) => void }) {
   );
 }
 
-class App extends React.Component<{}, { profile: Profile, searchQuery: string }> {
+
+function formatDate(date: Date): string {
+  return Intl.DateTimeFormat(navigator.language, { weekday: 'long', month: 'short', day: 'numeric' }).format(date);
+}
+
+function formatAchievement(achievement: Achievement): string {
+  return `${achievement.description}. Project: ${achievement.project.name}. Team: ${achievement.project.teamDisplayName}. Delievered: ${formatDate(achievement.dateOfDelivery)}`;
+}
+
+function AchievementView(props: { achievement: Achievement }): JSX.Element {
+  return <ListItem>
+    <ListItemText primary={formatAchievement(props.achievement)}></ListItemText>
+  </ListItem>
+}
+
+function AchievementList(props: { achievements: Achievement[] }): JSX.Element {
+  return <Paper style={{ maxHeight: 400, maxWidth: 400, overflow: "auto" }}><List>
+    {props.achievements
+      .sort(achievement => 0 - achievement.dateOfDelivery.getTime())
+      .map(achievement => <AchievementView key={achievement.id} achievement={achievement}></AchievementView>)
+    }
+  </List>
+  </Paper>
+}
+
+interface AppState {
+  profile: Profile,
+  searchQuery: string,
+  skillFilter: string
+}
+
+class App extends React.Component<{}, AppState> {
   constructor(props) {
     super(props);
     this.state = {
       profile: null,
       searchQuery: null,
+      skillFilter: null
     };
   }
 
   async handleChange(searchQuery: string) {
     const profile = await getProfile(searchQuery);
     this.setState({ profile: profile, searchQuery });
+  }
+
+  handleSkillClick(skill: string) {
+    this.setState({ skillFilter: skill });
   }
 
   render() {
@@ -112,10 +272,12 @@ class App extends React.Component<{}, { profile: Profile, searchQuery: string }>
       return <Grid container spacing={2}>
         {searchField}
         <Grid item xs={6}>
-          <ProfileView profile={this.state.profile}></ProfileView>
+          <ProfileView profile={this.state.profile} handleSkillClick={skill => this.handleSkillClick(skill)}></ProfileView>
         </Grid>
         <Grid item xs={6}>
-          <ProfileView profile={this.state.profile}></ProfileView>
+          <AchievementList achievements={this.state.skillFilter ?
+            this.state.profile.skills.find(skill => skill.name === this.state.skillFilter).achievements :
+            getLatestAchievements(this.state.profile)}></AchievementList>
         </Grid>
       </Grid>
     } else {
